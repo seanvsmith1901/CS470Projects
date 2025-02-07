c23ec12aba7c1a86f020f4817f7d13198e3cb3dd
seanvsmith1901 <seanvsmith1901@gmail.com>
2025-02-06 18:11:40 -0700

    APPARENTLY THIS DIDN"T GET ADDED
    so I lost the last hour or so of work. Its fine; I understand how it works better now. Just gonna get something to eat and redo it later.

---
+++ ReversiAgent/cs-470-reversi-python-client-master/README.md
@@ -0,0 +1,9 @@
+# cs-470-reversi-python-client
+
+A Python 3 client for the BYU CS 470 Reversi lab. All that you need to change for your project is the `make_move` function in the reversi_bot.py script. See comment there for information on useful functionality. 
+
+To run, enter directory where this code is located and type `python reversi_python_client.py localhost 1` if you want this running on localhost and for it to be first player. You can do 2 for second player or specify a different host as well.
+
+## PULL REQUESTS ARE WELCOME
+
+![Reversi Board Image](https://upload.wikimedia.org/wikipedia/commons/a/ae/Othello_%28Reversi%29_board.jpg)
---
+++ ReversiAgent/cs-470-reversi-python-client-master/reversi_bot.py
@@ -0,0 +1,31 @@
+import numpy as np
+import random as rand
+import reversi
+
+class ReversiBot:
+    def __init__(self, move_num):
+        self.move_num = move_num
+
+    def make_move(self, state):
+        '''
+        This is the only function that needs to be implemented for the lab!
+        The bot should take a game state and return a move.
+
+        The parameter "state" is of type ReversiGameState and has two useful
+        member variables. The first is "board", which is an 8x8 numpy array
+        of 0s, 1s, and 2s. If a spot has a 0 that means it is unoccupied. If
+        there is a 1 that means the spot has one of player 1's stones. If
+        there is a 2 on the spot that means that spot has one of player 2's
+        stones. The other useful member variable is "turn", which is 1 if it's
+        player 1's turn and 2 if it's player 2's turn.
+
+        ReversiGameState objects have a nice method called get_valid_moves.
+        When you invoke it on a ReversiGameState object a list of valid
+        moves for that state is returned in the form of a list of tuples.
+
+        Move should be a tuple (row, col) of the move you want the bot to make.
+        '''
+        valid_moves = state.get_valid_moves()
+
+        move = rand.choice(valid_moves) # Moves randomly...for now
+        return move
