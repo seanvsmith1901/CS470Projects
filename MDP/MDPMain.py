2e87389e22b1f43bb23023afb081c042ed6524c8
seanvsmith1901 <seanvsmith1901@gmail.com>
2025-03-18 21:27:10 -0600

    I've got the framework down
    work on the rest later? maybe?

--- MDP/MDPMain.py
+++ MDP/MDPMain.py
@@ -1,7 +1,7 @@
 # literally no clue what to pu there.
 import itertools # will create cartesian product for us
 import pandas as pd
-
+import numpy as np
 
 states_1 = ["death", "no_aneurysm", "<30", "30-35", "35-40", "40-45", "45-50", "50-55", "55-60", "60-65", "65-70", "70-75",  "75-80", ">80"]
 states_2 = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120]
@@ -12,7 +12,9 @@ gamma2 = 0.7 # thats LOW.
 def run_fetcher():
     states = cartesian_product(states_1, states_2)
     df30 = pd.read_csv("30-35 - Sheet1.csv")
-    df60 = pd.read_csv("60-35 - Sheet1.csv")
+    df60 = pd.read_csv("60-65 - Sheet1.csv")
+    # to access a data frame at an intended position, use df.loc["key1", "key2"] (in row, column order)
+    V = np.zeros(len(states)) # so that way we have the outcome of every possible policy
 
 
 
@@ -20,9 +22,15 @@ def run_fetcher():
 
 
 
-
-
-
+def reward_function(state):
+    health = state[0]
+    age = state[1]
+    if health == "no_aneurysm":
+        return 100 - age
+    elif health == "death":
+        return -100
+    else:
+        return 0.9 * (100 - age)
 
 def cartesian_product(arry1, arry2):
     return list(itertools.product(arry1, arry2))
