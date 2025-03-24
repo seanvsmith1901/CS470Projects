0d305ad5d8e71133448f38a231067952328a0865
seanvsmith1901 <seanvsmith1901@gmail.com>
2025-03-24 12:23:24 -0600

    This is desinged to be much more user freindly
    now we can specify stuff way easier and it autoloops through all of them. However, I am still not convinced it works. I might go through and invent my own data set to try and do it what I want to do.

--- MDP/MDPMain.py
+++ MDP/MDPMain.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 states_1 = ["Dead", "no AAA", "<30 mm", "30-35 mm", "35-40 mm", "40-45 mm", "45-50 mm", "50-55 mm", "55-60 mm", "60-65 mm", "65-70 mm", "70-75 mm",  "75-80 mm", "> 80 mm"]
-#states_2 = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120]
+
 states_30 = [30, 31, 32, 33, 34, 35]
 states_60 = [60, 61, 62, 63, 64, 65]
 
@@ -25,8 +25,8 @@ def run_fetcher(age, gamma): # our data frame of choice (30 by default)
         return
 
     # to access a data frame at an intended position, use df.loc["key1", "key2"] (in row, column order)
-    states = [(health, age) for health in states_1 for age in states_2]  # creates cartesian product bc I am lazy
-    state_indexs = {state: idx for idx, state in enumerate(states)}  # dictionary time (faster)
+    states = [(health, possible_age) for health in states_1 for possible_age in states_2]  # creates cartesian product bc I am lazy
+    state_indexes = {state: idx for idx, state in enumerate(states)}  # dictionary time (faster)
 
     V = np.zeros(len(states)) # so that way we have the outcome of every possible policy
     policy = {}
@@ -35,7 +35,7 @@ def run_fetcher(age, gamma): # our data frame of choice (30 by default)
         converged = True
         for state in states:
             health, age = state
-            state_index = state_indexs[state]
+            state_index = state_indexes[state]
             if health == "Dead":
                 V[state_index] = -100 # fixed reward here. will it help? no clue.
                 continue # nothing to calcualte here.
@@ -48,14 +48,12 @@ def run_fetcher(age, gamma): # our data frame of choice (30 by default)
                 next_age = age + 1
 
             reward = reward_function(state)
-            state_index = state_indexs[state]
             old_value = V[state_index]
 
-            best_value = float("-inf")
-            best_action = None
+            action_values = []
 
             for action in actions:
-                action_value = 0
+                action_value = []
                 if action == "surgery":
                     df = dfy
                 else:
@@ -63,35 +61,43 @@ def run_fetcher(age, gamma): # our data frame of choice (30 by default)
                 for new_health in states_1:
                     if new_health == "Dead":
                         new_state = ("Dead", age)
-                    elif new_health == health or (new_health == next_anuyerisn_size(health)):
-                        new_state = (new_health, next_age)
                     else:
-                        continue # skip invalid transitions
-
+                        next_health = next_anuyerisn_size(health)
+                        if (new_health == health or new_health == next_health):
+                            new_state = (new_health, next_age)
+                        else:
+                            continue
 
 
                     transition_prob = df.loc[health, new_health]
                     if transition_prob > 0:
-                        new_state_index = state_indexs[new_state]
-                        future_value = transition_prob * (reward + gamma * V[new_state_index])
-                        #print("here is the expected future value ", future_value)
-                        action_value += future_value
+                        #print(f"State: {state}, Action: {action}, Transition to {new_state} with prob {transition_prob}")
 
-                if action_value > best_value:
-                    best_value = action_value
-                    best_action = action
+                        new_state_index = state_indexes[new_state]
+                        future_value = transition_prob * (reward + gamma * V[new_state_index])
+                        action_value.append(future_value)
+
+                if action_value:
+                    best_action_value = max(action_value)
+                    action_values.append(best_action_value)
+                else:
+                    action_values.append(float("-inf"))
+
+            best_value = max(action_values)
+            best_action = actions[action_values.index(best_value)]
+
+            #print(f"State: {state}, Surgery Value: {action_values[0]}, Surveillance Value: {action_values[1]}")
 
             V[state_index] = best_value
             policy[state] = best_action
 
             # check for convergence
             if abs(best_value - old_value) > epsilon:
-                print("This is how close we were ", best_value - old_value)
-                converged = False
+                converged = False # this breaks
 
 
     print(V)
-    save_values_to_cvs(V, policy, states)
+    save_values_to_cvs(V, policy, states, age, gamma)
 
 def reward_function(state):
     health = state[0]
@@ -125,7 +131,8 @@ def next_anuyerisn_size(current_health):
     else:
         return current_health
 
-def save_values_to_cvs(V, policy, states, filename="VFor65.0.9.csv"):
+def save_values_to_cvs(V, policy, states, age, gamma):
+    filename = "Vfor" + str(age) + "." + str(gamma) + ".csv"
     df = pd.DataFrame({
         "Health" : [state[0] for state in states],
         "Age" : [state[1] for state in states],
@@ -137,10 +144,8 @@ def save_values_to_cvs(V, policy, states, filename="VFor65.0.9.csv"):
 
 
 if __name__ == '__main__':
-
-
-    gamma1 = 0.9
-    gamma2 = 0.7  # thats LOW.
-
-
-    run_fetcher(60, gamma1)
+    ages = [30]#, 60]
+    gammas = [0.9]#, 0.7]
+    for age in ages:
+        for gamma in gammas:
+            run_fetcher(age, gamma)
--- MDP/displayFile.py
+++ MDP/displayFile.py
@@ -7,27 +7,30 @@ from matplotlib.colors import ListedColormap
 import matplotlib.patches as mpatches  # Import for custom legend
 
 def display_plot():
-    df = pd.read_csv("VFor65.0.9.csv")
-    # pivot to make it easier for heatmaps, also drop the dead column.
-    df_policy = df[(df["Health"] != "Dead") & (df["Health"] != "no AAA")].pivot(index="Age", columns="Health", values="Best Action")
+    possible_dfs = ["VFor65.0.9.csv", "VFor65.0.7.csv", "VFor35.0.9.csv", "VFor35.0.7.csv"]
+    for df_file in possible_dfs:
+        df = pd.read_csv(df_file)
 
-    policy_colors = {"surgery" : 1, "surveillance" : 0}
-    df_numeric = df_policy.replace(policy_colors)
-    custom_map = ListedColormap(["blue", "red"])
+        # pivot to make it easier for heatmaps, also drop the dead column.
+        df_policy = df[(df["Health"] != "Dead") & (df["Health"] != "no AAA")].pivot(index="Age", columns="Health", values="Best Action")
 
-    # Create the heatmap
-    plt.figure(figsize=(12, 8))
-    sns.heatmap(df_numeric, cmap=custom_map, linewidths=0.5, cbar=False)
+        policy_colors = {"surgery" : 1, "surveillance" : 0}
+        df_numeric = df_policy.replace(policy_colors)
+        custom_map = ListedColormap(["blue", "red"])
 
-    plt.title("optimal policy heatmap")
-    plt.xlabel("Aneurysm Size")
-    plt.ylabel("Age")
+        # Create the heatmap
+        plt.figure(figsize=(12, 8))
+        sns.heatmap(df_numeric, cmap=custom_map, linewidths=0.5, cbar=False)
 
-    save_path = r"C:\Users\Sean\Documents\GitHub\CS470Projectrs\MDP\heatmap.png"
+        plt.title("optimal policy heatmap")
+        plt.xlabel("Aneurysm Size")
+        plt.ylabel("Age")
 
-    plt.savefig(save_path, dpi=300, bbox_inches="tight")
+        base_name = os.path.splitext(df_file)[0]
+        save_path = fr"C:\Users\Sean\Documents\GitHub\CS470Projectrs\MDP\heatmap_{base_name}.png"
 
-    plt.show()
+        plt.savefig(save_path, dpi=300, bbox_inches="tight")
+        plt.show()
 
 if __name__ == '__main__':
     display_plot()
