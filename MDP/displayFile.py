92284a38c498498b50d24b1683de2a17fd092e9c
seanvsmith1901 <seanvsmith1901@gmail.com>
2025-03-31 21:12:37 -0600

    changed the heatmaps
    minor problem in the legend, fixed it

--- MDP/displayFile.py
+++ MDP/displayFile.py
@@ -7,20 +7,25 @@ from matplotlib.colors import ListedColormap
 import matplotlib.patches as mpatches  # Import for custom legend
 
 def display_plot():
-    possible_dfs = ["VFor60.0.9.csv", "VFor60.0.7.csv", "VFor30.0.9.csv", "VFor30.0.7.csv"]
+    possible_dfs = ["Vfor60.0.9.csv", "Vfor60.0.7.csv", "Vfor30.0.9.csv", "Vfor30.0.7.csv"]
     for df_file in possible_dfs:
         df = pd.read_csv(df_file)
 
         # pivot to make it easier for heatmaps, also drop the dead column.
         df_policy = df[(df["Health"] != "Dead") & (df["Health"] != "no AAA")].pivot(index="Age", columns="Health", values="Best Action")
 
-        policy_colors = {"surgery" : 1, "surveillance" : 0}
+        policy_colors = {"surgery" : 0, "surveillance" : 1}
         df_numeric = df_policy.replace(policy_colors)
+        print(df_numeric)
         custom_map = ListedColormap(["blue", "red"])
 
         # Create the heatmap
         plt.figure(figsize=(12, 8))
-        sns.heatmap(df_numeric, cmap=custom_map, linewidths=0.5, cbar=False)
+        sns.heatmap(df_numeric, cmap=custom_map, linewidths=0.5, cbar=False, vmin=0, vmax=1)
+
+        surgery_patch = mpatches.Patch(color='blue', label='Surgery')
+        surveillance_patch = mpatches.Patch(color='red', label='Surveillance')
+        plt.legend(handles=[surgery_patch, surveillance_patch])
 
         plt.title("optimal policy heatmap")
         plt.xlabel("Aneurysm Size")
